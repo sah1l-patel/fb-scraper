780b3b626bc0073bbd467a4e48d444d8eeb84b00
sahil <sahil@cerebellumcapital.com>
2018-01-17 17:01:03 +0530

    added date field for end date

--- fb_scraper/forms.py
+++ fb_scraper/forms.py
@@ -1,10 +1,14 @@
 #importing forms
 from django import forms 
- 
+from datetime import datetime
+
 #creating our forms
 class SignupForm(forms.Form):
 	#django gives a number of predefined fields
 	#CharField and EmailField are only two of them
 	#go through the official docs for more field details
 	page = forms.CharField(label='Enter company/page name', max_length=100)
+
 	start_date = forms.DateField(label = 'Enter start date', widget=forms.SelectDateWidget(years=[y for y in range(1930,2050)]))
+	end_date = forms.DateField(label='Enter end date',
+								 widget=forms.SelectDateWidget(years=[y for y in range(1930, 2050)]))
