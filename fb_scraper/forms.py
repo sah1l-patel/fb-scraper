ce3efb67880381bd780b58310ff665d6344a39e3
sah1l <35501710+sah1l@users.noreply.github.com>
2018-01-25 01:32:12 +0530

    Update forms.py

--- fb_scraper/forms.py
+++ fb_scraper/forms.py
@@ -7,7 +7,7 @@ class SignupForm(forms.Form):
 	#django gives a number of predefined fields
 	#CharField and EmailField are only two of them
 	#go through the official docs for more field details
-	page = forms.CharField(label='Enter company/page name', max_length=100)
+	page = forms.CharField(label='URL', max_length=150)
 	current_year = datetime.datetime.today().year
 
 	start_date = forms.DateField(label = 'Enter start date', widget=forms.SelectDateWidget(years=[y for y in range(2004,current_year+1)]))
